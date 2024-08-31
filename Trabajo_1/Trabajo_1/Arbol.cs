ae1e7b08c1bdfbcbbbc77834de37fda8ff28cd6f
LovisottoSantiago <santiagolovisotto@gmail.com>
2024-08-30 21:53:24 -0300

    Initial commit

---
+++ Trabajo_1/Trabajo_1/Arbol.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Trabajo_1
+{
+	public class ArbolBinario<T>
+	{
+		
+		private T dato;
+		private ArbolBinario<T> hijoIzquierdo;
+		private ArbolBinario<T> hijoDerecho;
+	
+		
+		public ArbolBinario(T dato) {
+			this.dato = dato;
+		}
+		
+		//Getters	
+		public T getDatoRaiz() {
+			return this.dato;
+		}
+
+		public ArbolBinario<T> getHijoIzquierdo() {
+			return this.hijoIzquierdo;
+		}
+	
+		public ArbolBinario<T> getHijoDerecho() {
+			return this.hijoDerecho;
+		}
+		
+		//Metodos
+		public void agregarHijoIzquierdo(ArbolBinario<T> hijo) {
+			this.hijoIzquierdo=hijo;
+		}
+	
+		public void agregarHijoDerecho(ArbolBinario<T> hijo) {
+			this.hijoDerecho=hijo;
+		}
+	
+		public void eliminarHijoIzquierdo() {
+			this.hijoIzquierdo=null;
+		}
+	
+		public void eliminarHijoDerecho() {
+			this.hijoDerecho=null;
+		}
+	
+		public bool esHoja() {
+			return this.hijoIzquierdo==null && this.hijoDerecho==null;
+		}
+		
+		//Metodos enunciado punto 4
+		public bool incluye(T elemento) {
+			if (this.dato.Equals(elemento)) {
+                return true;
+            }
+            bool valorIzq = (this.hijoIzquierdo != null) && this.hijoIzquierdo.incluye(elemento); //Recursion
+            bool valorDer = (this.hijoDerecho != null) && this.hijoDerecho.incluye(elemento); //Recursion
+            return valorIzq || valorDer;  //True si al menos una de las condiciones es verdadera       
+		}
+	
+		public void inorden() {
+			if (this.hijoIzquierdo != null) {
+                this.hijoIzquierdo.inorden(); //Recursion parte izq
+            }
+			Console.Write(this.dato + " ");
+            if (this.hijoDerecho != null) {
+                this.hijoDerecho.inorden();
+            }
+		}
+		
+		public void preorden() {
+			Console.Write(this.dato + " ");
+			if (this.hijoIzquierdo != null) {
+				this.hijoIzquierdo.preorden();
+			}
+			if (this.hijoDerecho != null) {
+				this.hijoDerecho.preorden();
+			}
+		}
+		
+		public void postorden() {
+			if (this.hijoIzquierdo != null) {
+				this.hijoIzquierdo.postorden();
+			}
+			if (this.hijoDerecho != null) {
+				this.hijoDerecho.postorden();
+			}
+			Console.Write(this.dato + " ");
+		}
+	
+		public int contarHojas() {
+			return 0;
+		}
+		
+		public void recorridoEntreNiveles(int n,int m) {
+		}
+
+	
+	}
+}
---
+++ Trabajo_1/Trabajo_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Trabajo_1
+{
+
+	public class Program
+	{
+		public static void Main(String[] args){
+			
+	 		// Crear un árbol binario de ejemplo
+	        ArbolBinario<int> raiz = new ArbolBinario<int>(1);
+	        ArbolBinario<int> nodo2 = new ArbolBinario<int>(2);
+	        ArbolBinario<int> nodo3 = new ArbolBinario<int>(3);
+	        ArbolBinario<int> nodo4 = new ArbolBinario<int>(4);
+	        ArbolBinario<int> nodo5 = new ArbolBinario<int>(5);
+	
+	        // Construir el árbol
+	        raiz.agregarHijoIzquierdo(nodo2);
+	        raiz.agregarHijoDerecho(nodo3);
+	        nodo2.agregarHijoIzquierdo(nodo4);
+	        nodo2.agregarHijoDerecho(nodo5);
+	
+	        // Probar el método incluye
+	        Console.WriteLine("¿El árbol incluye al 3? " + raiz.incluye(3)); // True
+	        Console.WriteLine("¿El árbol incluye al 6? " + raiz.incluye(6)); // False
+	
+	        // Imprimir recorridos
+	        Console.WriteLine("\nRecorrido inorden:");
+	        raiz.inorden();
+	
+	        Console.WriteLine("\nRecorrido preorden:");
+	        raiz.preorden();
+	
+	        Console.WriteLine("\nRecorrido postorden:");
+	        raiz.postorden();
+	        
+	        Console.ReadKey();
+		}
+	}
+}
