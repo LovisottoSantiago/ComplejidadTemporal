f3fa23e50e209feaf97fe0519e9d6123e7e5fdb5
LovisottoSantiago <santiagolovisotto@gmail.com>
2024-09-01 16:48:24 -0300

    cambios

---
+++ Trabajo_2/Trabajo_2/Heap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_2 {
+
+
+    public class Heap<T> {
+
+        private T[] datos;
+        private bool queHeapEs; // True si es Max, False si es Min
+
+        public Heap(T[] datos, bool queHeapEs) {
+            this.datos = datos;
+            this.queHeapEs = queHeapEs;        
+        }
+
+        public T[] getDatos() { return datos; } 
+        public bool getQueHeapEs() { return queHeapEs; }
+
+        public bool esVacia() {
+            return getDatos().Length == 0;
+        }
+
+
+
+    }
+}
